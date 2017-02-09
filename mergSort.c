b3b042b03244427de4f7bf111474bc5e7adb8702
mohannad (from phone ) <waheedmohannad55578@gmail.com>
2017-02-10 01:59:37 +0300

    implement cut file method

--- mergSort.c
+++ mergSort.c
@@ -14,6 +14,10 @@ int cutFile(char *);
 
 int main ()
 {
+
+	char * m ="mojhh"; 
+	char * n= "lkjghhjj"+m;
+	printf(n);
 	return 0;
 }
 
@@ -22,10 +26,16 @@ int cutFile (char *file )
 
 	FILE * read ;
 	read =fopen (file,"rd");
+	size_t p;
 	int num_file=0;
 	struct student st ={0,"",""};
-	while (read()){
+	while ((p=fread(&st ,sizeof(struct student ),1,read))>0){
+		for (int i=0;i<3&& p>0;i++){
+		FILE * write ;
+		write =fopen ();
 
+		}
+	num_file++;
 	}
 	return num_file ;
 }
